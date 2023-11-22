3bfad5dfe912f75fd93bb73c05d0afb65cae2e29
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-11-21 20:59:39 -0600

    added comments fetches dad stores in allPokemons array

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,5 @@
 const MAX_POKEMON = 151;
+//variables storing references to various HTML elements in the document.
 const listWrapper = document.querySelector(".list-wrapper");
 const searchInput = document.querySelector("#search.input");
 const numberFilter = document.querySelector("#number");
@@ -7,6 +8,8 @@ const notFoundMessage = document.querySelector("#not-found-message");
 
 //Empty Array that stores all the pokemon when they are retrieved
 let allPokemons = [];
+
+//Promise Handling:.then() is used twice to handle the response asynchronously: The first converts the response to JSON. The second receives the JSON data and assigns the results property to the allPokemons array.
 
 fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
 .then((response) => response.json())
