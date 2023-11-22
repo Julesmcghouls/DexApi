efac71994e0242c2552be07055af01278193ae6a
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-11-21 20:23:22 -0600

    Linked Javascript and Started JS Pokemon file

---
+++ pokemon.js
@@ -0,0 +1,11 @@
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search.input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+//Empty Array that stores all the pokemon when they are retrieved
+let allPokemons = [];
+
+
