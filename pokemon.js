13f076fa25789eb44da4d51add9c7a325d270fc5
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-11-23 19:28:14 -0600

    error with name and id display

--- pokemon.js
+++ pokemon.js
@@ -1,68 +1,67 @@
-// Variables storing references to various HTML elements in the document
+const MAX_POKEMON = 151;
+//variables storing references to various HTML elements in the document.
 const listWrapper = document.querySelector(".list-wrapper");
-const searchInput = document.querySelector("#search-input"); // Selects the search input by its ID
+const searchInput = document.querySelector("#search.input");
 const numberFilter = document.querySelector("#number");
 const nameFilter = document.querySelector("#name");
 const notFoundMessage = document.querySelector("#not-found-message");
 
-// Empty Array that stores all the Pokemon when they are retrieved
+//Empty Array that stores all the pokemon when they are retrieved
 let allPokemons = [];
 
-// Fetching data from the Pokemon API
+//Promise Handling:.then() is used twice to handle the response asynchronously: The first converts the response to JSON. The second receives the JSON data and assigns the results property to the allPokemons array.
+
 fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
-  .then((response) => response.json()) // Convert the response to JSON
-  .then((data) => {
-    allPokemons = data.results; // Store fetched Pokemon data
-    displayPokemons(allPokemons); // Display the fetched Pokemon
-  });
+.then((response) => response.json())
+.then((data) => {
+    allPokemons = data.results;
+    //retrieves 
+    displayPokemons(allPokemons);
+});
 
-// Async function - fetches additional data for a specific Pokemon ID
+//async - run code and then delay until we retreive the data we want
 async function fetchPokemonDataBeforeRedirect(id) {
-  try {
-    // Destructuring - fetch data for a specific Pokemon and its species
-    const [pokemon, pokemonSpecies] = await Promise.all([
-      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json()),
-      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => res.json()),
-    ]);
-    return true; // Data fetched successfully
-  } catch (error) {
-    console.error("Failed to fetch Pokemon data before redirect");
-  }
+    //destructoring - creating several consts in one go by putting them in arrays
+    try {
+        //Promise you will receive some data 
+        const [pokemon, pokemonSpecies] = await Promise.all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+            res.json()
+        ), 
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+            res.json()
+        ),    
+    ])
+    return true
+    } catch (error) {
+        console.error("Failed to fetch Pokemon data before redirect");
+    }
 }
+function displayPokemons(pokemon) {
+    listWrapper.innerHTML = " ";
 
-// Function to display fetched Pokemon data
-function displayPokemons(pokemons) {
-  listWrapper.innerHTML = ""; // Clear previous content
-
-  pokemons.forEach((pokemon) => {
-    const pokemonId = pokemon.url.split("/")[6]; // Extract Pokemon ID from URL
-    const listItem = document.createElement("div"); // Create a new list item element
-    listItem.className = "list-item"; // Assign CSS class to the list item
-
-    // Populate the list item with Pokemon information
-    listItem.innerHTML = `
-      <div class="number-wrap">
-        <p class="caption-fonts">${pokemonId}</p>
-      </div>
-      <div class="image-wrap">
+    pokemon.forEach((pokemon) => {
+        const pokemonId = pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className = "list-item";
+        listItem.innerHTML = `
+        
+        <div class="number-wrap">
+            <p class="caption-fonts"><${pokemonId}/p>
+        </div>
+        <div class="image-wrap">
         <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg" alt="${pokemon.name}" />
-      </div>
-      <div class="name-wrap">
-        <p class="body3-fonts">${pokemon.name}</p>
-      </div>`;
+    </div>
+    <div class="name-wrap">
+    <p class="body3-fonts"><#${pokemon.name}/p>
+</div>`;
 
-    // Add a click event listener to each Pokemon list item
-    listItem.addEventListener("click", async () => {
-      const success = await fetchPokemonDataBeforeRedirect(pokemonId); // Fetch additional data
-      if (success) {
-        window.location.href = `./detail.html?id=${pokemonId}`; // Redirect to Pokemon detail page
-      }
+listItem.addEventListener("click", async () => {
+    const success = await fetchPokemonDataBeforeRedirect(pokemonId);
+    if (success) {
+        window.location.href = `./detail.html?id=${pokemonId}`
+    }
+});
+
+listWrapper.appendChild(listItem);
     });
-
-    listWrapper.appendChild(listItem); // Append the list item to the list wrapper
-  });
 }
-
-
-
-//@48:02 I need to fix some things 
